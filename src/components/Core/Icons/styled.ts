7813c38fdc6c09ea6d1e2ec3b15f7dd0c86b7ddf
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-20 21:38:20 -0300

    FEA : Add icon component

---
+++ src/components/Core/Icons/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  cursor: pointer;
+  border: 2px none/ blue;
+  width: min-content;
+  display: flex;
+  width: fit-content;
+`
