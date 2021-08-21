243f635f54aa8a6ea37567294c3df0736e9a3ee8
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-20 21:42:18 -0300

    FEA : add env var

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  env: {
+    REPO: 'luisguilher.me',
+    RESUME: 'resume'
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
