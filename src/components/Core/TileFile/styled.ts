4c85820f527f0bf4774c10299ccf457177816b71
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-20 21:38:53 -0300

    FEA : Add fileTIle component

---
+++ src/components/Core/TileFile/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import Arrow from 'public/icons/arrow.svg'
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: max-content max-content max-content;
+  grid-gap: 5px;
+  padding: 1px;
+  padding-right: 10px;
+  padding-left: 10px;
+  align-items: center;
+`
+export const ArrowContainer = styled.div``
+
+export const ArrowIcon = styled(Arrow)`
+  transform: ${({ open }) => !open && 'rotate(-90deg)'};
+  fill: ${({ theme }) => theme.colors.text};
+`
