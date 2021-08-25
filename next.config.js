5212716df9952471452e236068735475f611da02
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-25 20:30:32 -0300

    ENH : change shaBranch env var to main

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   env: {
     REPO: 'luisguilher.me',
     RESUME: 'resume',
-    shaBranch: '7892492bd6579182bd098df204ca700bae3afea8'
+    shaBranch: 'main'
   },
   reactStrictMode: true,
   webpack(config) {
