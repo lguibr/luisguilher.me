d47e3c04a4f0c2ca513f91ef7255b267c7ab7e81
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-27 14:30:52 -0300

    FEA : duplicate files to source

--- src/components/Home/SideBar/Sections/Source/index.tsx
+++ src/components/Home/SideBar/Sections/Source/index.tsx
@@ -1,5 +1,21 @@
-const Source: React.FC = () => {
-  return <div>Source!</div>
+import useContextFile from 'src/hooks/useContextFile'
+import RenderDir from './RenderDir'
+import Text from 'src/components/Core/Text'
+import { Container, Title } from './styled'
+const Files: React.FC = () => {
+  const { treeFiles } = useContextFile()
+
+  return (
+    treeFiles && (
+      <Container>
+        <Title>
+          <Text size={12}>EXPLORER</Text>
+        </Title>
+
+        <RenderDir embedded={0} files={treeFiles} />
+      </Container>
+    )
+  )
 }
 
-export default Source
+export default Files
---
+++ src/components/Home/SideBar/Sections/Source/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  overflow: auto;
+  height: 100%;
+  box-sizing: border-box;
+`
+
+export const Title = styled.div`
+  padding: 4px 8px;
+`
