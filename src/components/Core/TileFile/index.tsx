c7147784268d22f33219ccd890fc44e983161dac
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-27 19:09:25 -0300

    FEA : apply useExtension on tileFIle and detect diff

--- src/components/Core/TileFile/index.tsx
+++ src/components/Core/TileFile/index.tsx
@@ -1,23 +1,7 @@
-import { File as FileType } from 'src/contexts/FileContext'
-import { Container, ArrowIcon, ArrowContainer } from './styled'
+import { FileType } from 'src/contexts/FileContext'
+import { Container, ArrowIcon, ArrowContainer, Span } from './styled'
 import Text from 'src/components/Core/Text'
-
-import TypescriptIcon from 'public/icons/typescript.svg'
-import TypescriptAltIcon from 'public/icons/typescript-outlined.svg'
-import EslintIcon from 'public/icons/eslint.svg'
-import BabelIcon from 'public/icons/babel.svg'
-import ImageIcon from 'public/icons/file-image-solid.svg'
-import FolderIcon from 'public/icons/folder-solid.svg'
-import OpenFolderIcon from 'public/icons/folder-open-solid.svg'
-import GitIcon from 'public/icons/git-icon.svg'
-import JSONIcon from 'public/icons/json.svg'
-import JavascriptIcon from 'public/icons/javascript.svg'
-import PrettierIcon from 'public/icons/prettier.svg'
-import ReactIcon from 'public/icons/react.svg'
-import YarnIcon from 'public/icons/yarn.svg'
-import YamlIcon from 'public/icons/yaml.svg'
-import NodeIcon from 'public/icons/nodejs.svg'
-import FileIcon from 'public/icons/file.svg'
+import { useExtension } from 'src/hooks/useExtension'
 
 export type FileTileProps = {
   file: FileType
@@ -26,37 +10,9 @@ export type FileTileProps = {
 }
 
 const FileTile: React.FC<FileTileProps> = ({ file, folder, open }) => {
-  const icons = [
-    { expression: /.png|.jpg|.jpeg|.svg|.ico/, icon: ImageIcon },
-    { expression: 'package.json', icon: NodeIcon },
-    { expression: 'git', icon: GitIcon },
-    { expression: 'eslint', icon: EslintIcon },
-    { expression: 'babel', icon: BabelIcon },
-    { expression: 'yarn', icon: YarnIcon },
-    { expression: 'prettier', icon: PrettierIcon },
-    { expression: '.d.ts', icon: TypescriptAltIcon },
-    { expression: '.json', icon: JSONIcon },
-    { expression: /.(t|j)sx/, icon: ReactIcon },
-    { expression: '.js', icon: JavascriptIcon },
-    { expression: '.ts', icon: TypescriptIcon },
-    { expression: /.yml|.yaml/, icon: YamlIcon },
-    { expression: /./, icon: FileIcon },
-    { expression: /./, icon: OpenFolderIcon },
-    { expression: /./, icon: FolderIcon }
-  ]
-
-  const IconByExtensionMatch = icons.find(({ expression }) =>
-    file?.name?.match(expression)
-  )
-
-  const selectedIcon = folder
-    ? open
-      ? OpenFolderIcon
-      : FolderIcon
-    : IconByExtensionMatch?.icon
-
-  const Icon = selectedIcon || FileIcon
-
+  const { extractIcon } = useExtension()
+  const Icon = extractIcon(file, open)
+  const { diff } = file
   return (
     <Container>
       <ArrowContainer>
@@ -64,6 +20,7 @@ const FileTile: React.FC<FileTileProps> = ({ file, folder, open }) => {
       </ArrowContainer>
       <Icon height="14px" width="14px" />
       <Text size={14}>{file?.name}</Text>
+      {diff && <Text size={13}>M</Text>}
     </Container>
   )
 }
--- src/components/Core/TileFile/styled.ts
+++ src/components/Core/TileFile/styled.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import Arrow from 'public/icons/arrow.svg'
+import TextComponent from '../Text'
 
+export const Span = styled(TextComponent)``
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: max-content max-content max-content;
+  grid-template-columns: max-content max-content max-content max-content;
   grid-gap: 5px;
   padding: 1px;
   padding-right: 10px;
