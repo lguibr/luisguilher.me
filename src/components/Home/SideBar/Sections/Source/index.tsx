7ad61a7a203341b3ec1f3859a232424aca1c74c7
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-28 18:11:37 -0300

    FEA : create source component

--- src/components/Home/SideBar/Sections/Source/index.tsx
+++ src/components/Home/SideBar/Sections/Source/index.tsx
@@ -1,22 +1,70 @@
 import useContextFile from 'src/hooks/useContextFile'
 import RenderDir from './RenderDir'
 import Text from 'src/components/Core/Text'
-import { Container, Title } from './styled'
+
+import List from 'public/icons/list.svg'
+import Tree from 'public/icons/tree.svg'
+
+import {
+  Container,
+  Title,
+  Row,
+  RowClickable,
+  IconContainer,
+  Ballon,
+  ArrowIcon
+} from './styled'
+import { useTree } from 'src/hooks/useTree'
+
+import { FileType } from 'src/contexts/FileContext'
+import { useState } from 'react'
+
 const Files: React.FC = () => {
-  const { files } = useContextFile()
+  const { build, rebuildPaths } = useTree()
+  const { diffFiles } = useContextFile()
 
-  const diffFiles = files.filter(({ diff }) => diff)
+  const [type, setType] = useState<'tree' | 'list'>('list')
+  const [open, setOpen] = useState<boolean>(true)
 
-  console.log({ diffFiles })
+  const repo = process.env.REPO || 'luisguilher.me'
+  const splittedDiffFiles = rebuildPaths(diffFiles)
+  const absoluteDiffTree = build(splittedDiffFiles)
+
+  const resumeFiles = absoluteDiffTree.filter(({ path }) => path === 'resume')
+  const repoFiles = absoluteDiffTree.filter(({ path }) => path !== 'resume')
+
+  const relativeDiffTree: FileType[] = repoFiles.length
+    ? [{ path: repo, name: repo, children: repoFiles }, ...resumeFiles]
+    : [...resumeFiles]
+
+  const sourceFiles = type === 'tree' ? relativeDiffTree : diffFiles
+  const totalChanges = JSON.stringify(diffFiles.length || 0)
+
+  const toggleTypeList = () => {
+    setType(type === 'list' ? 'tree' : 'list')
+  }
 
   return (
-    diffFiles && (
+    sourceFiles && (
       <Container>
-        <Title>
-          <Text size={12}>EXPLORER</Text>
-        </Title>
-
-        <RenderDir embedded={0} files={diffFiles} />
+        <Row>
+          <Title>
+            <Text size={12}>SOURCE CONTROL</Text>
+          </Title>
+          <IconContainer onClick={toggleTypeList}>
+            {type === 'list' ? <Tree /> : <List />}
+          </IconContainer>
+        </Row>
+        <RowClickable onClick={() => setOpen(!open)}>
+          <Row>
+            <ArrowIcon height="10px" width="10px" open={open} />
+            <Text size={12}>Changes </Text>
+          </Row>
+          <Ballon>
+            <Text size={12}>{totalChanges} </Text>
+          </Ballon>
+        </RowClickable>
+        {open && <RenderDir embedded={0} files={sourceFiles} />}
       </Container>
     )
   )
