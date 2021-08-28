2cba9a968fc8d80a50ae4640fab2d026eb20109c
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2021-08-28 18:09:39 -0300

    ENH : pass sideContext to root app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,22 @@ import { UIProvider } from 'src/contexts/ThemeContext'
 import { PrintProvider } from 'src/contexts/PrintContext'
 import Shell from 'src/components/Core/Shell'
 import GlobalStyle from 'src/styles/global'
+import { SideBarProvider } from 'src/contexts/SideBarContext'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <GlobalStyle />
       <UIProvider>
-        <FileProvider>
-          <PrintProvider>
-            <Shell>
-              <Component {...pageProps} />
-            </Shell>
-          </PrintProvider>
-        </FileProvider>
+        <SideBarProvider>
+          <FileProvider>
+            <PrintProvider>
+              <Shell>
+                <Component {...pageProps} />
+              </Shell>
+            </PrintProvider>
+          </FileProvider>
+        </SideBarProvider>
       </UIProvider>
     </>
   )
